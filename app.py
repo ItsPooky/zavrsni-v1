018294e93e8d8ca4fac295d51b1fe84af4be2a11
ItsPooky <sstankovic0111@gmail.com>
2023-09-10 19:09:12 +0200

    napravismo ga

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ import mysql.connector
 connection = mysql.connector.connect(
     passwd="root", # password for the database
     user="root", # username
-    database="cv", # database name     
+    database="cv", # database name
     port=3306, # port on which the mysql server is running 
-    auth_plugin='mysql_native_password' # if you are using mysql 8.x  
+    auth_plugin='mysql_native_password' # if you are using mysql 8.x
 )
 
 cursor = connection.cursor(dictionary=True)
@@ -16,11 +16,8 @@ app = Flask(__name__)
 
 app.secret_key = 'stefan'
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET', 'POST'])
 def render_home_page():
-    return render_template('index.html')
-
-def insert_data():
     if request.method == 'POST':
         first_name = request.form['first_name']
         last_name = request.form['last_name']
@@ -29,6 +26,29 @@ def insert_data():
         zip_code = request.form['zip_code']
         phone_number = request.form['phone_number']
         city = request.form['city']
-        
+
+        # Insert data into the database (You should sanitize and validate data before inserting)
+        cursor.execute(
+            "INSERT INTO your_table_name (first_name, last_name, email, address, zip_code, phone_number, city) "
+            "VALUES (%s, %s, %s, %s, %s, %s, %s)",
+            (first_name, last_name, email, address, zip_code, phone_number, city)
+        )
+        connection.commit()
+
+        # Redirect to the next page
+        return redirect(url_for('render_history_page'))
+
+    return render_template('index.html')
+
+@app.route('/2', methods=['GET', 'POST'])
+def render_history_page():
+    if request.method == 'POST':
+        return redirect(url_for('history_page'))  # Redirect to the 'history_page' route
+    return render_template('history.html')
+
+@app.route('/history')
+def history_page():
+    return render_template('history.html')
+
 app.run(debug = True)
-connection.close() 
+connection.close()
